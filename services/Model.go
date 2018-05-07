0f128d9983e56178450fd56dd347d1520f973e1b
NiveditaDeveloper <39053647+NiveditaDeveloper@users.noreply.github.com>
2018-05-07 18:08:26 +0530

    Update Model.go

--- services/Model.go
+++ services/Model.go
@@ -33,8 +33,6 @@ func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
 	log.Println(w, message)
 }
 
-
-
 func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)

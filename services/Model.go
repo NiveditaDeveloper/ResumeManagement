d7e7542c02c186eaf18dda016521f4a40e5b1abd
NiveditaDeveloper <39053647+NiveditaDeveloper@users.noreply.github.com>
2018-05-07 18:57:52 +0530

    Update Model.go

--- services/Model.go
+++ services/Model.go
@@ -6,24 +6,25 @@ import (
 )
 
 type ResumeDetails struct {
-	Id            		string  `json:"id"`
+	Id            		string `json:"id"`
+	//candidate details will be moved to candidate collection  and address details will be moved to address collection(in process)
 	CandidateFirstName 	string `json:"candidate_first_name"`
-	CandidateMiddleName string `json:"candidate_middle_name"`
+	CandidateMiddleName 	string `json:"candidate_middle_name"`
 	CandidateLastName 	string `json:"candidate_last_name"`
-	Expertise 			string `json:"candidate_expertise"`
-	InterviewStatus 	string `json:"interview_status"`
-	CurrentCompany 		string `json:"current_company"`
-	CurrentDesignation 	string `json:"current_designation"`
+	ContactNumber 		string `json:"contact_number"`
+	EmailId 		string `json:"email_id"`
+	Expertise 		string `json:"candidate_expertise"`
 	CurrentLocation 	string `json:"current_location"`
 	PrefferedLocation	string `json:"preffered_location"`
+	CurrentCompany 		string `json:"current_company"`
+	CurrentDesignation 	string `json:"current_designation"`
 	NoticePeriod 		string `json:"notice_period"`
-	ModeOfInterview 	string `json:"mode_of_interview"`
-	ContactNumber 		string `json:"contact_number"`
-	EmailId 			string `json:"email_id"`
-	Experience 			string `json:"experience"`
-	CurrentCTC 			float64 `json:"current_ctc"`
+	Experience 		string `json:"experience"`
+	CurrentCTC 		float64 `json:"current_ctc"`
 	ExpectedCTC 		float64 `json:"expected_ctc"`
-	ResFile 			string `json:"url"`
+	ResFile 		string `json:"url"`
+	InterviewStatus 	string `json:"interview_status"`
+	ModeOfInterview 	string `json:"mode_of_interview"`
 }
 
 func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
